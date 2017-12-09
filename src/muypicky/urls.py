7ff529312feec18128e0c88c6ef527cf98c73149
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-12-08 20:44:57 -0500

    templates and views

--- src/muypicky/urls.py
+++ src/muypicky/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import home
+from restaurants.views import home, ContactView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home)
+    url(r'^$', home),
+    url(r'^contact/$', ContactView.as_view()),
 ]
--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html', {})
+
+class ContactView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'contact.html', context)
